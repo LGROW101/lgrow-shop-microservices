82dcd8275c0def50367d4bf081b7e49889c56136
LGROW <hengsahatsawat091@gmail.com>
2024-01-10 15:20:27 +0700

    Save items

--- modules/item/itemMode.go
+++ modules/item/itemMode.go
@@ -1,1 +1,33 @@
 package item
+
+import "github.com/LGROW101/LGROW-Microservices/modules/models"
+
+type (
+	CreateItem struct {
+		Title    string `json:"title" validate:"required,max-64"`
+		Price    string `json:"price" validate:"required"`
+		Damage   int    `json:"damage" validate:"required"`
+		ImageUrl string `json:"image_url" validate:"image_url"`
+	}
+
+	ItemShowCase struct {
+		ItemId   string  `json:"item_id"`
+		Title    string  `json:"title"`
+		Price    float64 `json:"price"`
+		Damage   int     `json:"damage"`
+		ImageUrl string  `json:"image_url"`
+	}
+	ItemSearchReq struct {
+		Title string `json:"title"`
+		models.PaginateReq
+	}
+	ItemUpdateReq struct {
+		Title    string  `json:"title" validate:"required,max=64"`
+		Price    float64 `json:"price" validate:"required"`
+		ImageUrl string  `json:"image_url" validate:"required,max=255"`
+		Damage   int     `json:"damage" validate:"required"`
+	}
+	EnableOrDisableItemReq struct {
+		UsageStatus bool `json:"_usege_status"`
+	}
+)
