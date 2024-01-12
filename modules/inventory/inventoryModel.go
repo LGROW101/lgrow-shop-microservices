dacd070af5352e64a32ef5c1859da6ba75414ca2
LGROW <hengsahatsawat091@gmail.com>
2024-01-12 11:26:53 +0700

    Save

--- modules/inventory/inventoryModel.go
+++ modules/inventory/inventoryModel.go
@@ -1,1 +1,21 @@
 package inventory
+
+import (
+	"github.com/LGROW101/LGROW-Microservices/modules/item"
+	"github.com/LGROW101/LGROW-Microservices/modules/models"
+)
+
+type (
+	UpdateInventoryReq struct {
+		PlayerId string `json:"player_id" validate:"required,max=64"`
+		ItemId   string `json:"item_id" validate:"required,max=64"`
+	}
+	ItemInInventory struct {
+		InventoryId string `json:"inventory_id"`
+		*item.ItemShowCase
+	}
+	PlayerInventory struct {
+		PlayerId string `json:"player_id"`
+		*models.PaginateRes
+	}
+)
