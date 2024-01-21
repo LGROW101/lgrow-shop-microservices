e4a5b8c49dfba4c89c609ad82c6395003c0870bd
LGROW <hengsahatsawat091@gmail.com>
2024-01-21 15:34:59 +0700

    Save

--- modules/auth/authRepository/authRepository.go
+++ modules/auth/authRepository/authRepository.go
@@ -2,7 +2,13 @@ package authRepository
 
 import (
 	"context"
+	"errors"
+	"log"
+	"time"
 
+	playerPb "github.com/LGROW101/LGROW-Microservices/modules/player/playerPb"
+	"github.com/LGROW101/LGROW-Microservices/pkg/grpccon"
+	"github.com/LGROW101/LGROW-Microservices/pkg/jwtauth"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,4 +24,23 @@ func NewAuthRepository(db *mongo.Client) AuthRepositoryService {
 }
 func (r *authRepository) authDbConn(pctx context.Context) *mongo.Database {
 	return r.db.Database("auth_db")
+}
+func (r *authRepository) CredentialSearch(pctx context.Context, grpcUrl string, req *playerPb.CredentialSearchReq) (*playerPb.PlayerProfile, error) {
+	ctx, cancel := context.WithTimeout(pctx, 30*time.Second)
+	defer cancel()
+
+	jwtauth.SetApiKeyInContext(&ctx)
+	conn, err := grpccon.NewGrpcClient(grpcUrl)
+	if err != nil {
+		log.Printf("Error: gRPC connection failed: %s", err.Error())
+		return nil, errors.New("error: gRPC connection failed")
+	}
+
+	result, err := conn.Player().CredentialSearch(ctx, req)
+	if err != nil {
+		log.Printf("Error: CredentialSearch failed: %s", err.Error())
+		return nil, errors.New("error: email or password is incorrect")
+	}
+
+	return result, nil
 }
